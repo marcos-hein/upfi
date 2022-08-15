6b86defd6468a63ace3a61f075ea5bfc1036934d
Marcos Pereira <marcos.hen-ext@via.com.br>
2022-08-15 10:48:15 -0300

    :sparkles: loading images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,18 +18,78 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    async ({ pageParam = null }) => {
+      const params = { after: pageParam };
+      const response = await api.get('/images', { params });
+      return response.data;
+    },
+    {
+      getNextPageParam: lastPage => (lastPage.after ? lastPage.after : null),
+    }
   );
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const content = {
+      pages: [
+        {
+          data: [
+            {
+              title: 'Doge',
+              description: 'The best doge',
+              url: 'https://i.ibb.co/K6DZdXc/minh-pham-LTQMgx8t-Yq-M-unsplash.jpg',
+              ts: 1620222828340000,
+              id: '294961059684418048',
+            },
+            {
+              title: 'Cachorrinho gif',
+              description: 'A Gracie é top',
+              url: 'https://i.ibb.co/r3NbmgH/ezgif-3-54a30c130cef.gif',
+              ts: 1620222856980000,
+              id: '295991055792210435',
+            },
+            {
+              title: 'React',
+              description: 'Dan Abramov',
+              url: 'https://i.ibb.co/864qfG3/react.png',
+              ts: 1620223108460000,
+              id: '295991069654385154',
+            },
+            {
+              title: 'Ignite',
+              description: 'Wallpaper Celular',
+              url: 'https://i.ibb.co/DbfGQW5/1080x1920.png',
+              ts: 1620223119610000,
+              id: '295991085899973123',
+            },
+            {
+              title: 'Ignite',
+              description: 'Wallpaper PC 4k',
+              url: 'https://i.ibb.co/fvYLKFn/3840x2160.png',
+              ts: 1620223133800000,
+              id: '295991107279389188',
+            },
+            {
+              title: 'Paisagem',
+              description: 'Sunset',
+              url: 'https://i.ibb.co/st42sNz/petr-vysohlid-9fqw-Gq-GLUxc-unsplash.jpg',
+              ts: 1620223149390000,
+              id: '295991128736399874',
+            },
+          ],
+          after: '295991160078336512',
+        },
+      ],
+      pageParams: [null],
+    };
+
+    // return data?.pages.map(page => page.data).flat();
+    console.log(content?.pages.map(page => page.data).flat());
+    return content?.pages.map(page => page.data).flat();
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) return <Loading />;
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) return <Error />;
 
   return (
     <>
@@ -38,6 +98,15 @@ export default function Home(): JSX.Element {
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {!hasNextPage && (
+          <Button
+            onClick={() => fetchNextPage()}
+            isLoading={isFetchingNextPage}
+            loadingText="Carregando..."
+          >
+            Carregar mais
+          </Button>
+        )}
       </Box>
     </>
   );
